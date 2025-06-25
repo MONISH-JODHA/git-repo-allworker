09505c0d5bd2e4778ac329d4a7705ebf2ad900b9
MONISH-JODHA <monish.21jics202@jietjodhpur.ac.in>
2025-06-25 15:25:44 +0530

    Convert remaining submodules to normal folders

---
+++ aws-three-tier-web-architecture-workshop/application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1-instance-1.ck9oyaw269ig.us-east-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : '2rskipepsi',
+    DB_DATABASE : 'mywebappdb'
+});ls -l /home/ec2-user/app-tier/index.js
---
+++ sample-java-code/src/main/java/com/ck/DemoApplication.java
@@ -0,0 +1,26 @@
+package com.ck;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@RestController
+@SpringBootApplication
+public class DemoApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DemoApplication.class);
+    }
+
+    @GetMapping
+    public String test() {
+        return "HELLO WORLD";
+    }
+}
